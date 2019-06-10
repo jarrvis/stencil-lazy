68824253235c47f14b23f6c4c49c402a1a4cf285
kai8wz <michal.kaliszewski@pl.bosch.com>
2019-06-10 17:57:15 +0200

    move st-lazy to stencil-lazy

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { getElement } from "@stencil/core";
+import { ComponentInstance } from "@stencil/core/dist/declarations";
+
+declare type LazyDecorator = (target: ComponentInstance, propertyKey: string) => void
+
+/**
+ * Call this function as soon as the element is inside the viewport.
+ * @param margin Optionally provide the padding (rootMargin) for IntersectionObserver. Determines how far from the viewport lazy loading starts. Can have values similar to the CSS margin property, e.g. "10px 20px 30px 40px" (top, right, bottom, left). The values can be percentages 
+ * @example
+```
+@Lazy()
+lazyCallback() {
+  // this will run when element is inside the viewport.
+}
+```
+ * @example
+```
+@Lazy({margin: "100px"})
+lazyCallback() {
+  // this will run when element is 100px from the viewport.
+}
+```
+ */
+export function Lazy(options?: LazyOptions): LazyDecorator {
+  return (proto: ComponentInstance, methodName: string) => {
+    const { componentDidLoad  } = proto;
+    proto.componentDidLoad  = function() {
+      const host = getElement(this);
+      const method = this[methodName];
+      const margin = options ? options.margin : '';
+      registerLazy(host, method, margin);
+      return componentDidLoad && componentDidLoad.call(this);
+    };
+  };
+}
+
+/**
+ * Register callback function for HTMLElement to be executed when the element is inside the viewport.
+ *
+ */
+export function registerLazy(element: HTMLElement, callback: () => void, marginProp?: string): void {
+
+  if ("IntersectionObserver" in window) {
+    const margin = getValidMargin(marginProp)
+    if (!margin) {
+      throw new Error(
+        "@Lazy() decorator's optional parameter 'margin' is given but not valid. It should be a string like CSS margin property, e.g. '10px 20px 30px 40px'(top, right, bottom, left) or just '10px' (all). The values can be percentages "
+      );
+    }
+    let io = new IntersectionObserver(
+      (data: any) => {
+        if (data[0].isIntersecting) {
+          callback.call(this);
+          io.disconnect();
+          io = null;
+        }
+      },
+      { rootMargin: margin }
+    );
+    io.observe(element);
+  } else {
+    // fall back to setTimeout for Safari and IE
+    setTimeout(() => {
+      callback.call(this);
+    }, 300);
+  }
+}
+
+/**
+ * Checs if margin has values like CSS margin property, e.g. "10px 20px 30px 40px" (top, right, bottom, left). The values can be percentages.
+ * For empty input string it returns default value '0px'. For not valid input it returns null.
+ * @param margin Determines how far from the viewport lazy loading starts
+ */
+export function getValidMargin(margin?): string {
+  const marginString = margin || '0px';
+  return marginString.split(/\s+/).every((margin) => {
+      /^(-?\d*\.?\d+)(px|%)$/.exec(margin);
+    }) ? marginString : null;
+}
+
+export interface LazyOptions {
+  margin?: string;
+}
